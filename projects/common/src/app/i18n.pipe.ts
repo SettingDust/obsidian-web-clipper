39ee0ab228faa5fbaa63ea373030bea1d76619ac
SettingDust <settingdust@gmail.com>
2022-03-11 17:00:09 +0800

    feat: protocol alert in options page fix #12

--- projects/common/src/app/i18n.pipe.ts
+++ projects/common/src/app/i18n.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class I18nPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): string {
-    const result = browser.i18n.getMessage(value)
+    const result = browser.i18n.getMessage(value, args)
     return result?.length ? result : value
   }
 }
